f2e461edf8fe0bcaa98fccec4922bea46078e0de
Aishwarya Roy <aishwaryaroy@Aishwaryas-MacBook-Pro.local>
2026-02-23 18:01:14 +0530

    Add: Python learning exercises Day 1-16

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator\n")
+userInputCity = input("Enter the city you grew up\n")
+userInputPet = input("Enter the pet you grew up\n")
+print("Your band name could be : " + userInputCity+ " " + userInputPet)
+
+
+
