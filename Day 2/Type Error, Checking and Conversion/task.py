cfb2932fa04e7e32d05668b75fb40d03f5448199
Aishwarya11roy <royaishwarya11@gmail.com>
2026-03-02 20:18:23 +0530

    Add: Day 2 Tip Calculator project

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,7 @@
-len(12345)
+print(len("12345"))
+print(type(34))
+print(type(34.6))
+print(type(True))
+
+letterNum = len(input("Enter your name"))
+print("Number of letters in your name: " + str(letterNum))
